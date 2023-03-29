a8a1962697abf9f97920a86a6e34ae116b51f408
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-29 00:33:04 -0400

    array filter lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (drivers, string) => {
+
+    const emptyDrivers = []
+    for (let driver of drivers) {
+        if (driver.toLowerCase () === string.toLowerCase())
+        emptyDrivers.push(driver)
+    }
+    return emptyDrivers;
+}
+
+function fuzzyMatch(drivers, letters) {
+    let lengthOfLetters = letters.length;
+    return drivers.filter(function (driverName) {
+      return driverName.slice(0, lengthOfLetters) === letters;
+    });
+  }
+
+
+
+const matchName = (drivers, name) => {
+    const emptyDrivers = []
+    for (let driver of drivers) {
+      if (driver.name === name)
+      emptyDrivers.push(driver)
+    }
+    return emptyDrivers;
+  }
